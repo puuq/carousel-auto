987b1213faa85ed2e3d4c28fb3e6d8bbe897ea86
San Choo <littlecono1@gmail.com>
2023-12-26 12:42:49 +0545

    changes

--- script.js
+++ script.js
@@ -33,3 +33,87 @@ var TrandingSlider = new Swiper('.tranding-slider', {
     },
   },
 });
+
+var swiperTest = new Swiper('.swiper-container-test', {
+  slidesPerView: 2,
+  spaceBetween: 10,
+  pagination: {
+    el: '.swiper-pagination-test',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next-test',
+    prevEl: '.swiper-button-prev-test',
+  },
+  on: {
+    slideChangeTransitionStart: function () {
+      var prevButton = document.querySelector('.swiper-button-prev-test');
+      var nextButton = document.querySelector('.swiper-button-next-test');
+
+      // Check if there is a next slide
+      if (swiperTest.isEnd) {
+        nextButton.style.display = 'none'; // Hide next button
+      } else {
+        nextButton.style.display = 'block'; // Show next button
+      }
+
+      // Check if there is a previous slide
+      if (swiperTest.isBeginning) {
+        prevButton.style.display = 'none'; // Hide prev button
+      } else {
+        prevButton.style.display = 'block'; // Show prev button
+      }
+    }
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const carouselContainer = document.querySelector('.carousel-container');
+  const prevBtn = document.querySelector('.prev-btn');
+  const nextBtn = document.querySelector('.next-btn');
+
+  let currentIndex = 0;
+
+  const users = [
+    {
+      name: 'John Doe',
+      rating: 4.5,
+      image: 'user1.jpg',
+    },
+    {
+      name: 'Jane Smith',
+      rating: 5.0,
+      image: 'user2.jpg',
+    },
+    // Add more user data as needed
+  ];
+
+  function updateCarousel() {
+    const user = users[currentIndex];
+    const userContent = `
+      <div class="user-image" style="background-image: url('${user.image}')"></div>
+      <div class="user-details">
+        <h3>${user.name}</h3>
+        <p>Rating: ${user.rating}</p>
+      </div>
+    `;
+
+    carouselContainer.innerHTML = userContent;
+  }
+
+  function showNextUser() {
+    currentIndex = (currentIndex + 1) % users.length;
+    updateCarousel();
+  }
+
+  function showPrevUser() {
+    currentIndex = (currentIndex - 1 + users.length) % users.length;
+    updateCarousel();
+  }
+
+  nextBtn.addEventListener('click', showNextUser);
+  prevBtn.addEventListener('click', showPrevUser);
+
+  // Initial update
+  updateCarousel();
+});
